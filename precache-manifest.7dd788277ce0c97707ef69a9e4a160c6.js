ee88f245d9220249a9ded4f2102853f79b5e6ac1
Shaonkabir <31365575+Shaonkabir@users.noreply.github.com>
2019-08-15 06:56:23 -0700

    Optimized Production build of Catch Of The Day

---
+++ precache-manifest.7dd788277ce0c97707ef69a9e4a160c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88845bfc42421905da62767ef67d6006",
+    "url": "/catchOfTheDay/index.html"
+  },
+  {
+    "revision": "da250c8c14243f66268e",
+    "url": "/catchOfTheDay/static/css/main.b55db1fc.chunk.css"
+  },
+  {
+    "revision": "d7fbb1af71b81db164fe",
+    "url": "/catchOfTheDay/static/js/2.6c726221.chunk.js"
+  },
+  {
+    "revision": "da250c8c14243f66268e",
+    "url": "/catchOfTheDay/static/js/main.e4886fa5.chunk.js"
+  },
+  {
+    "revision": "b5887a9a49b300247d60",
+    "url": "/catchOfTheDay/static/js/runtime~main.78d7a69a.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/catchOfTheDay/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/catchOfTheDay/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/catchOfTheDay/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/catchOfTheDay/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "c175fe49ca6c599199a81c6791490344",
+    "url": "/catchOfTheDay/static/media/blanch_caps_inline-webfont.c175fe49.svg"
+  },
+  {
+    "revision": "47e26dee9b053b540478750bdf7a87d5",
+    "url": "/catchOfTheDay/static/media/haymaker-webfont.47e26dee.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/catchOfTheDay/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/catchOfTheDay/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/catchOfTheDay/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
